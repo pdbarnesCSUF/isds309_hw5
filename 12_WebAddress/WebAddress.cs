8553b76e68b3a6b853661ede0332f5bc919b4b5c
pdbarnesCSUF <pdbarnes@csu.fullerton.edu>
2018-10-30 17:13:01 -0700

    done 12

--- 12_WebAddress/WebAddress.cs
+++ 12_WebAddress/WebAddress.cs
@@ -26,7 +26,17 @@ namespace _12_WebAddress
             WriteLine("hw5#12 - Patrick Barnes");
             WriteLine("WebAddress");
 
-
+            string in_name = "";
+            string noSpaces = "";
+            Write("Business name:");
+            in_name = ReadLine();
+            //strip spaces
+            for (int i = 0; i < in_name.Length; ++i)
+            {
+                if (in_name[i] != ' ')
+                    noSpaces += in_name[i];
+            }
+            WriteLine("www.{0}.com",noSpaces);
 
             WriteLine("--End--");
         }
