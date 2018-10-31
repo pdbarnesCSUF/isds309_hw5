47691bd340de94cd19c08934a21fb164e568ec98
pdbarnesCSUF <pdbarnes@csu.fullerton.edu>
2018-10-30 17:22:00 -0700

    done 13

--- 13_CountVowels/CountVowels.cs
+++ 13_CountVowels/CountVowels.cs
@@ -25,7 +25,28 @@ namespace _13_CountVowels
             WriteLine("hw5#13 - Patrick Barnes");
             WriteLine("CountVowels");
 
+            string in_phrase = "";
+            int vowelCount = 0;
+            char checkLetter = '\0';
 
+            Write("Enter Phrase: ");
+            in_phrase = ReadLine();
+
+            for (int i = 0; i < in_phrase.Length; ++i)
+            {
+                checkLetter = char.ToUpper(in_phrase[i]);
+                switch(checkLetter)
+                {
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                        vowelCount++;
+                        break;
+                }
+            }
+            WriteLine("Vowels :" + vowelCount);
 
             WriteLine("--End--");
         }
